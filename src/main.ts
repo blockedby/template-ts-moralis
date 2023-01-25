3038317ca0d03fdd00783c7b5ab6ab5f13238d66
Alex Green <kasnis12@gmail.com>
2023-01-25 15:41:56 +0100

    changes

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,18 @@
 import express from "express";
-import Moralis from "moralis";
-import { EvmChain } from "@moralisweb3/evm-utils";
 import dotenv from "dotenv";
 
+import { MoralisRouter } from "./moralis";
+
 dotenv.config();
+
 const app = express();
 
 app.get("/", function (req, res) {
     res.send("Hello World");
 });
 
-let moralisStarted = false;
+app.use("/moralis", MoralisRouter)
 
-app.post("/start-moralis", async function (req, res) {
-    try {
-        await Moralis.start({
-            apiKey: process.env.MORALIS_API_KEY,
-        });
-        console.log("Moralis started");
-        moralisStarted = true;
-    } catch (error) {
-        console.log("Moralis not started. \n" + error);
-        res.status(400).send(JSON.stringify(error));
-    }
-    res.send("Moralis started");
-});
-
-app.post("/moralis/openStream", async function (req, res) {
-    if (moralisStarted) {
-        try {
-            console
-            const streamConfig = {
-                chains: [EvmChain.ETHEREUM],
-                description: "Test stream",
-                tag: "test",
-                webhookUrl: process.env.WEBHOOK_URL || "" + "/hook/open-stream",
-            }
-            console.log("Stream opened");
-            res.status(200).send(JSON.stringify(req.params));
-        } catch (error) {
-            console.log("Stream not opened. \n" + error);
-            res.status(400).send(JSON.stringify(error));
-        }
-    } else {
-        res.status(200).send(JSON.stringify("Moralis not started"));
-    }
-})
 
 console.log("Server is running on port 3000");
 app.listen(3000);
--- src/moralis.ts
+++ src/moralis.ts
@@ -1,12 +1,53 @@
-// import Moralis from 'moralis';
+import express from "express";
+import Moralis from "moralis";
+import { EvmChain } from "@moralisweb3/evm-utils";
+import * as Order from "./resources/protocol/artifacts/contracts/Order/Order.sol/Order.json";
 
-// async function initMoralis() {
-//     await Moralis.start({
-//         apiKey: 'YOUR_API_KEY',
-//       });
-//       Moralis.initi
-// }
+const router = express.Router();
 
-// export default initMoralis;
-  
-  
+let moralisStarted = false;
+
+router.post("/login", async function (req, res) {
+    try {
+        await Moralis.start({
+            apiKey: process.env.MORALIS_API_KEY,
+        });
+        console.log("Moralis started");
+        moralisStarted = true;
+    } catch (error) {
+        console.log("Moralis not started. \n" + error);
+        res.status(400).send(JSON.stringify(error));
+    }
+    res.send("Moralis started");
+});
+
+router.post("/open-order-stream", async function (req, res) {
+    if (moralisStarted) {
+        try {
+            console.log(req.body);
+            const streamConfig = {
+                chains: [EvmChain.ETHEREUM],
+                description: "Monitor Order events",
+                tag: "order",
+                abi: Order.abi,
+                includeContractLogs: true,
+                topics0: [
+                    "CallbackCreated(uint256,address)",
+                    "Lended(uint256,address,uint256)",
+                    "Repayed(uint256,address,uint256)",
+                ],
+
+                webhookUrl: process.env.WEBHOOK_URL || "" + "/hook/open/order",
+            };
+            console.log("Stream opened");
+            res.status(200).send(JSON.stringify(req.params));
+        } catch (error) {
+            console.log("Stream not opened. \n" + error);
+            res.status(400).send(JSON.stringify(error));
+        }
+    } else {
+        res.status(400).send(JSON.stringify("Moralis not started"));
+    }
+});
+
+export { router as MoralisRouter };
