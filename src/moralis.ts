6283897b0a6dcaa9c4eae40c2f512600011dcd0e
Alex Green <kasnis12@gmail.com>
2023-01-25 14:41:35 +0100

    added types

---
+++ src/moralis.ts
@@ -0,0 +1,12 @@
+// import Moralis from 'moralis';
+
+// async function initMoralis() {
+//     await Moralis.start({
+//         apiKey: 'YOUR_API_KEY',
+//       });
+//       Moralis.initi
+// }
+
+// export default initMoralis;
+  
+  
