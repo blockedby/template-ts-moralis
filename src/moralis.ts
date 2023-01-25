f795a26d75e122372bed056f0dedf3b9a07fc4a6
Alex Green <kasnis12@gmail.com>
2023-01-25 16:20:24 +0100

    catch hook error

--- src/moralis.ts
+++ src/moralis.ts
@@ -5,15 +5,12 @@ import * as Order from "./resources/protocol/artifacts/contracts/Order/Order.sol
 
 const router = express.Router();
 
-let moralisStarted = false;
-
 router.post("/login", async function (req, res) {
     try {
         await Moralis.start({
             apiKey: process.env.MORALIS_API_KEY,
         });
         console.log("Moralis started");
-        moralisStarted = true;
     } catch (error) {
         console.log("Moralis not started. \n" + error);
         res.status(400).send(JSON.stringify(error));
@@ -21,33 +18,49 @@ router.post("/login", async function (req, res) {
     res.send("Moralis started");
 });
 
-router.post("/open-order-stream", async function (req, res) {
-    if (moralisStarted) {
-        try {
-            console.log(req.body);
-            const streamConfig = {
-                chains: [EvmChain.ETHEREUM],
-                description: "Monitor Order events",
-                tag: "order",
-                abi: Order.abi,
-                includeContractLogs: true,
-                topics0: [
-                    "CallbackCreated(uint256,address)",
-                    "Lended(uint256,address,uint256)",
-                    "Repayed(uint256,address,uint256)",
-                ],
+router.post("/stream/order/create", async function (req, res) {
+    try {
+        console.log(req.body);
+        const streamConfig = {
+            chains: [EvmChain.GOERLI],
+            description: "Monitor Order events [GOERLI]",
+            tag: "order",
+            abi: Order.abi,
+            includeContractLogs: true,
+            topic0: [
+                "CallbackCreated(uint256,address)",
+                "Lended(uint256,address,uint256)",
+                "Repayed(uint256,address,uint256)",
+            ],
+            //0x0c5a7d625337b6f7e1cee193ce2c68a6420bfd67
+            webhookUrl: process.env.WEBHOOK_URL || "" + "/hook/open/order",
+        };
+        // Moralis SDK Core Error: [C0006] Request failed, Bad Request(400):
+        // Could not POST to https://easy-hook-catcher.vercel.app/api/webhook. Please check your webhook URL.
 
-                webhookUrl: process.env.WEBHOOK_URL || "" + "/hook/open/order",
-            };
-            console.log("Stream opened");
-            res.status(200).send(JSON.stringify(req.params));
-        } catch (error) {
-            console.log("Stream not opened. \n" + error);
-            res.status(400).send(JSON.stringify(error));
-        }
-    } else {
-        res.status(400).send(JSON.stringify("Moralis not started"));
+
+        const stream = await Moralis.Streams.add(streamConfig);
+        console.log("Stream opened");
+        res.status(200).send(JSON.stringify(stream));
+    } catch (error) {
+        console.log("Stream not opened. \n" + error);
+        res.status(400).send(JSON.stringify(error));
     }
 });
+
+router.post("/stream/order/addAddress", async function (req, res) {
+    try {
+        const resp = await Moralis.Streams.addAddress({
+            id: req.body.stream.id,
+            address: req.body.address,
+        });
+        res.status(200).send(JSON.stringify(resp.toJSON()));
+    } catch (error) {
+        console.log("Stream not opened. \n" + error);
+        res.status(400).send(JSON.stringify(error));
+    }
+});
+
+router;
 
 export { router as MoralisRouter };
